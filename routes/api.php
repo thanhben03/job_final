23ac47bf1ae19c9852e1e23ebbe03a58fde9ee0c
= <=>
2024-12-09 19:39:18 +0700

    remove auth sanctum middleware

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,7 @@ use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 
-Route::prefix('api/v1')->middleware('auth:sanctum')->group(function () {
+Route::prefix('api/v1')->group(function () {
 
     Route::prefix('/jobs')->group(function () {
 
