2b25e72f9ac4a57b49dfc4017a7a0400292201d1
thanhben03 <nben19733@gmail.com>
2025-03-16 14:05:51 +0700

    update redirect URLs to use environment variables for OAuth services

--- config/services.php
+++ config/services.php
@@ -37,16 +37,16 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://127.0.0.1:8000/auth/google/callback',
+        'redirect' => env('GOOGLE_CALLBACK_URL'),
     ],
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => 'http://127.0.0.1:8000/auth/github/callback',
+        'redirect' => env('GITHUB_CALLBACK_URL'),
     ],
     'facebook' => [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect' => 'http://127.0.0.1:8000/auth/github/callback',
+        'redirect' => env('FACEBOOK_CALLBACK_URL'),
     ],
 ];
